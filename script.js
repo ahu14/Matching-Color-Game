ee95c558788f2f9312ebf9effc3ea057a1875efe
ahu14 <sherlyhu28@gmail.com>
2022-11-04 15:24:14 +0700

    the basic version of the game

--- script.js
+++ script.js
@@ -1,25 +1,62 @@
-let inputHue = document.querySelector('input#hue');
-let labelHue = document.querySelector('label#hue');
-labelHue.innerHTML = inputHue.value;
+let inputRed = document.querySelector('input#red');
+let labelRed = document.querySelector('label#red');
 
-let inputSaturation = document.querySelector('input#saturation');
-let labelSaturation = document.querySelector('label#saturation');
-labelSaturation.innerHTML = inputSaturation.value;
+let inputBlue = document.querySelector('input#blue');
+let labelBlue = document.querySelector('label#blue');
 
-let inputLightness = document.querySelector('input#lightness');
-let labelLightness = document.querySelector('label#lightness');
-labelLightness.innerHTML = inputLightness.value;
+let inputGreen = document.querySelector('input#green');
+let labelGreen = document.querySelector('label#green');
+
+labelRed.innerHTML = inputRed.value;
+labelBlue.innerHTML = inputBlue.value;
+labelGreen.innerHTML = inputGreen.value;
 
 
-let colorBox = document.querySelector('.color-box');
-function setBackground(){
-    colorBox.style.background = `hsl(${inputHue.value}, ${inputSaturation.value}%, ${inputLightness.value}%)`
+let colorBox = document.querySelectorAll('.color-box');
+
+function randomNumber(){
+    return Math.floor(Math.random() * 255);
+}
+
+function setRandomColor(){
+    let red = randomNumber();
+    let green = randomNumber();
+    let blue = randomNumber();
+
+    colorBox[0].style.setProperty(
+        'background-color', 
+        `rgb(${red}, ${green}, ${blue})`
+    )
+
+    return {
+        'red' : red,
+        'green' : green,
+        'blue' : blue
+    }
+}
+
+let colors = setRandomColor();
+
+function checkColors(colors){
+    return (
+        inputRed.value == colors.red &&
+        inputGreen.value == colors.green &&
+        inputBlue.value == colors.blue
+    )
 }
 
 function slider(event){
     let slider = event.target;
     let sliderLabel = document.querySelector(`label#${slider.id}`)
+    
+    colorBox[1].style.setProperty(
+        'background-color', 
+        `rgb(${inputRed.value}, ${inputGreen.value}, ${inputBlue.value})`
+    )
 
     sliderLabel.innerHTML = slider.value;
-    setBackground();
+
+    if (checkColors(colors)){
+        setRandomColor();
+    }
 }
