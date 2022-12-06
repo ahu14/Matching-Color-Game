daece4e164d7b8496383b8c8f13fdd3f9f97b25c
ahu14 <sherlyhu28@gmail.com>
2022-12-06 16:57:50 +0700

    new version

--- script/index.js
+++ script/index.js
@@ -1,20 +1,84 @@
-let homeBox = document.querySelector('.home-box');
-let formBox = document.querySelector('.form-box');
+let colorBox = document.querySelector('.color-box');
+let randomValue = () => Math.floor(Math.random() * 255);
 
 
-function startGame(){
-    homeBox.style.setProperty('display', 'none');
-    homeBox.style.setProperty('pointer-events', 'none');
 
-    formBox.style.setProperty('display', 'grid');
-    formBox.style.setProperty('pointer-events', 'auto');
+function randomColor(){
+    let red = randomValue();
+    let green = randomValue();
+    let blue = randomValue();
+
+    colorBox.style.background = `rgb(${red}, ${green}, ${blue})`;
+    
+    return {
+        'red' : red,
+        'green' : green,
+        'blue' : blue
+    }
+}
+let getColor = randomColor();
+getColor;
+
+
+
+let score = 0;
+let scoreHTML = document.querySelector('#score');
+let range = document.querySelector('.range');
+let number = document.querySelector('#number');
+let inputBox = document.querySelector('.input-box');
+
+inputBox.style.background = `rgb(${range.value}, 
+    ${getColor.green}, ${getColor.blue})`;
+number.innerHTML = range.value;
+
+scoreHTML.innerText = score;
+
+
+
+function inputt(){
+    if (getColor.red == range.value){
+        setTimeout(() => {
+            getColor = randomColor();
+
+            score++;
+            scoreHTML.innerText = score;
+        }, 200);
+    }
+
+    number.innerHTML = range.value;
+    inputBox.style.background = `rgb(${range.value}, 
+        ${getColor.green}, ${getColor.blue})`;
 }
 
-function sendData(event){
-    event.preventDefault();
-    
-    let data = document.querySelector('select#difficulty');
-    sessionStorage.setItem('difficulty', data.value);
+let plus = () => {
+    range.value = parseInt(range.value) + 1;
+    inputt();
+}
 
-    window.location.replace("http://127.0.0.1:5500/game.html");
+let minus = () => {
+    range.value = parseInt(range.value) - 1;
+    inputt();
+}
+
+
+
+let addBtn = document.querySelector('.add-clickBtn');
+let minusBtn = document.querySelector('.minus-clickBtn');
+let btnArray = [addBtn, minusBtn];
+let functionArray = [plus, minus];
+let timeStamp;
+let time = 0;
+
+for (let i = 0; i < 2; i++){
+    btnArray[i].addEventListener('mousedown', () => {
+        timeStamp = setInterval(() => functionArray[i](), 50);
+    })
+
+    btnArray[i].addEventListener('mouseup', () => {
+        clearInterval(timeStamp);
+    })
+
+    btnArray[i].addEventListener('mousemove', () => {
+        clearInterval(timeStamp);
+    })
 }
