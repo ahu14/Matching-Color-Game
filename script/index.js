a6d7757b05df195e900420f68a2e5d55ad57d4e6
ahu14 <sherlyhu28@gmail.com>
2022-12-15 20:19:08 +0700

    added time attack

--- script/index.js
+++ script/index.js
@@ -22,7 +22,7 @@ getColor;
 
 
 let score = 0;
-let scoreHTML = document.querySelector('#score');
+let scoreHTML = document.querySelectorAll('#score');
 let range = document.querySelector('.range');
 let number = document.querySelector('#number');
 let inputBox = document.querySelector('.input-box');
@@ -31,7 +31,12 @@ inputBox.style.background = `rgb(${range.value},
     ${getColor.green}, ${getColor.blue})`;
 number.innerHTML = range.value;
 
-scoreHTML.innerText = score;
+let showScore = () => {
+    for (let i of scoreHTML){
+        i.innerHTML = score;
+    }
+}
+showScore();
 
 
 
@@ -41,7 +46,7 @@ function inputt(){
             getColor = randomColor();
 
             score++;
-            scoreHTML.innerText = score;
+            showScore();
         }, 200);
     }
 
@@ -62,23 +67,68 @@ let minus = () => {
 
 
 
-let addBtn = document.querySelector('.add-clickBtn');
-let minusBtn = document.querySelector('.minus-clickBtn');
-let btnArray = [addBtn, minusBtn];
+let btnArray = document.querySelectorAll('[class$="-clickBtn"]');
 let functionArray = [plus, minus];
 let timeStamp;
-let time = 0;
 
-for (let i = 0; i < 2; i++){
-    btnArray[i].addEventListener('mousedown', () => {
-        timeStamp = setInterval(() => functionArray[i](), 50);
-    })
-
-    btnArray[i].addEventListener('mouseup', () => {
-        clearInterval(timeStamp);
-    })
-
-    btnArray[i].addEventListener('mousemove', () => {
-        clearInterval(timeStamp);
-    })
+let rememberTime = (i) => {
+    timeStamp = setInterval(() => {
+        let time = timeHtml.innerHTML;
+        time >= 0 ? functionArray[i]() : deleteTime();
+    }, 70);
 }
+
+let deleteTime = () => {
+    return clearInterval(timeStamp);
+}
+
+
+
+let notifBar = document.querySelector('.notif-bar');
+let timeHtml = document.querySelector('#time');
+let time = 60;
+let frame;
+
+let refresh = () => {
+    console.log('clicked');
+    window.location.reload();
+}
+
+
+let timeCounter = () => {
+    if (time <= 0){
+        plus = () => {}
+        minus = () => {}
+        range.oninput = () => {}
+        range.disabled = "true";
+
+        for (let i = 0; i < 2; i++){
+            btnArray[i].onmousedown = () => deleteTime();
+            btnArray[i].onmouseup = () => {};
+            btnArray[i].onmousemove = () => {};
+        }
+
+        time = 0;
+        notifBar.style.display = "flex";
+        notifBar.style.pointerEvents = "auto";
+        showScore();
+        cancelAnimationFrame(timeCounter);
+    }
+
+    timeHtml.innerHTML = time;
+    frame = requestAnimationFrame(timeCounter);
+
+    if (frame == 60){
+        for (let i = 0; i < 2; i++){
+            btnArray[i].onmousedown = () => rememberTime(i);
+            btnArray[i].onmouseup = () => deleteTime();
+            btnArray[i].onmousemove = () => deleteTime();
+        }
+    }
+
+    else if (frame % 60 == 1){
+        time--;
+    }
+}
+
+requestAnimationFrame(timeCounter);
