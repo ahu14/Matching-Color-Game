75ae5522cedd5ab37eaf19d6db92d5a2093a4401
ahu14 <sherlyhu28@gmail.com>
2023-01-29 11:52:02 +0700

    make the game more interesting

--- script/index.js
+++ script/index.js
@@ -1,128 +1,144 @@
-let btn = document.querySelector('.button');
-let clickInfo = {
-    numClick : 1,
-    clicked : false
-}
+import {pages, editStatus, updateStatus} from "./pages.js";
+updateStatus();
 
-let editClicked = () => {
-    btn.animate([
-        { 'height' : '40px'}
-    ], { 
-        duration : 75,
-        iterations : 1
-    });
+let btnInstruction = document.querySelector('.btn-box-instruction');
+btnInstruction.onclick = function(){
+    editStatus('.box-play');
 
-    if (clickInfo.numClick > 0){
-        !clickInfo.clicked ? 
-            clickInfo.clicked = true : 
-            clickInfo.clicked = false;
+    let btn = document.querySelector('.button');
+    let clickInfo = {
+        numClick : 1,
+        clicked : false
     }
 
-    clickInfo.numClick -= 1;
-}
+    let editClicked = () => {
+        btn.animate([
+            { 'height' : '40px'}
+        ], { 
+            duration : 75,
+            iterations : 1
+        });
 
-btn.addEventListener('click', editClicked);
-
-
-
-let colorList = ['orange', 'pink', 'blue', 'red', 'green'];
-
-let getRandomColor = () => {
-    return colorList[Math.floor(Math.random() * colorList.length)];
-}
-
-let colorData = {
-    text : '',
-    color : '',
-    click : false
-}
-
-
-let startGame = () => {
-    let displayText = document.querySelector('#display-text');
-    displayText.innerHTML = getRandomColor();
-    colorData.text = displayText.innerHTML;
-
-    displayText.style.color = getRandomColor();
-    colorData.color = displayText.style.color;
-
-    if (colorData.text == colorData.color){
-        colorData.click = true;
-    }
-
-    else{
-        colorData.click = false;
-    }
-}
-
-let refresh = () => {
-    clickInfo.clicked = false;
-    startTime = undefined;
-    clickInfo.numClick = 1;
-}
-
-
-let gameOver = document.querySelector('.game-over');
-let btnGameOver = document.querySelector('.btn-game-over');
-let lose = (time, btn, box) => {
-    if (lastTime == undefined){
-        lastTime = time - startTime;
-    };
-
-    if (time - startTime - lastTime > 400 && time - startTime - lastTime < 420){
-        for (let i of box.children){
-            i.style.display = "none";
+        if (clickInfo.numClick > 0){
+            !clickInfo.clicked ? 
+                clickInfo.clicked = true : 
+                clickInfo.clicked = false;
         }
 
-        gameOver.style.display = 'flex';
-        btnGameOver.onclick = () => window.location.reload();
+        clickInfo.numClick -= 1;
     }
- }
+
+    btn.addEventListener('click', editClicked);
 
 
 
-let score = document.querySelector('#score');
-let box = document.querySelector('.box');
-let scoreNum = 0;
+    let colorList = ['orange', 'pink', 'blue', 'red', 'green'];
 
-let startTime;
-let lastTime;
-function play(time){
-    if (startTime == undefined){
-        startTime = time;
+    let getRandomColor = () => {
+        return colorList[Math.floor(Math.random() * colorList.length)];
+    }
+
+    let colorData = {
+        text : '',
+        color : '',
+        click : false
+    }
+
+
+    let startGame = () => {
+        let displayText = document.querySelector('#display-text');
+        displayText.innerHTML = getRandomColor();
+        colorData.text = displayText.innerHTML;
+
+        displayText.style.color = getRandomColor();
+        colorData.color = displayText.style.color;
+
+        colorData.text == colorData.color ? 
+            colorData.click = true : 
+            colorData.click = false;
+    }
+
+    let refresh = () => {
+        clickInfo.clicked = false;
+        clickInfo.numClick = 1;
+        startTime = undefined;
+    }
+
+    let addScore = () => {
+        scoreNum += 1;
         score.innerHTML = scoreNum;
-        startGame();
     }
 
-    if (time - startTime > 4000){
-        if (clickInfo.clicked == colorData.click){
-            scoreNum += 1;
+
+    let btnGameOver = document.querySelector('.btn-game-over');
+    let lose = (btn, box, totalTime) => {
+        setTimeout(() => {
+            editStatus('.box-game-over');
+        }, 150);
+
+        btnGameOver.onclick = () => {
+            scoreNum = 0;
             score.innerHTML = scoreNum;
             refresh();
-        }
 
-        else{
-            btn.removeEventListener('click', editClicked);
-            lose(time, btn, box);
-        }
+            pause = false;
+            totalTime = 4000;
+            editStatus('.box-play');
+            play();
+        };
     }
 
-    else{
-        if (clickInfo.numClick < 1){
-            if (clickInfo.clicked !== colorData.click){
-                btn.removeEventListener('click', editClicked);
-                lose(time, btn, box);
+
+
+    let score = document.querySelector('#score');
+    let box = document.querySelector('.box');
+    let scoreNum = 0;
+
+    let startTime;
+    let totalTime = 4000;
+    const minusTime = 0.3;
+
+    let pause = false;
+    function play(time){
+        if (startTime == undefined){
+            startTime = time;
+            score.innerHTML = scoreNum;
+            startGame();
+        }
+
+        if (time - startTime > totalTime){
+            if (clickInfo.clicked == colorData.click){
+                addScore();
+                refresh();
+                totalTime > 1500 ? totalTime -= minusTime : totalTime;
+            }
+
+            else{
+                pause = true;
             }
         }
 
-        if (clickInfo.clicked && colorData.click){
-            scoreNum += 1;
-            score.innerHTML = scoreNum;
-            refresh();
+        else{
+            if (clickInfo.numClick < 1){
+                if (clickInfo.clicked !== colorData.click){
+                    pause = true;
+                }
+            }
+
+            if (clickInfo.clicked && colorData.click){
+                addScore();
+                refresh();
+                totalTime > 1500 ? totalTime -= minusTime : totalTime;
+            }
         }
+
+        if (pause) {
+            lose(btn, box, totalTime);
+            return;
+        }
+
+        requestAnimationFrame(play);
     }
 
-    requestAnimationFrame(play);
+    play();
 }
-
-requestAnimationFrame(play);
---
+++ script/pages.js
@@ -0,0 +1,25 @@
+export let pages = {
+    '.box-instruction' : 'flex',
+    '.box-play' : 'none',
+    '.box-game-over' : 'none'
+}
+
+export function editStatus(target){
+    if (target != null){
+        pages[target] = 'flex';
+
+        for (let a in pages){
+            if (a != target){
+                pages[a] = 'none';
+            }
+        }
+
+        updateStatus();
+    }
+}
+
+export function updateStatus(){
+    for (let i in pages){
+        document.querySelector(i).style.display = `${pages[i]}`;
+    }
+}
